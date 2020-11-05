a69eeb2bc64c8f61b41d17ba264a72b473cef9c3
ahd <ahdhani0@gmail.com>
2020-11-05 12:31:06 +0300

    chore: add gradient background to body

--- utils/GlobalStyles.tsx
+++ utils/GlobalStyles.tsx
@@ -10,5 +10,8 @@ export default createGlobalStyle`
         margin:0;
         font-size:16px;
         font-family: 'Poppins', sans-serif;
+        background: radial-gradient(67.33% 101.9% at 51.53% 32.67%, rgba(255, 255, 255, 0) 0%, ${({
+          theme: { colors },
+        }) => colors.grey} 100%);
     }
 `;
