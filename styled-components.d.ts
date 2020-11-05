e3bf14716443aed8a53164337e1415fe234c496c
ahd <ahdhani0@gmail.com>
2020-11-05 10:43:28 +0300

    chore: setup theme

---
+++ styled-components.d.ts
@@ -0,0 +1,5 @@
+import theme from './utils/theme';
+type ThemeInterface = typeof theme;
+declare module 'styled-components' {
+    interface DefaultTheme extends ThemeInterface {}
+}
---
+++ utils/theme.ts
@@ -0,0 +1,77 @@
+export interface Space {
+  xs: string;
+  sm: string;
+  md: string;
+  lg: string;
+  xl: string;
+}
+
+export interface FontSizes {
+  xs: string;
+  sm: string;
+  md: string;
+  lg: string;
+  xl: string;
+}
+export interface Colors {
+  black: string;
+  red: string;
+  blue: string;
+  white: string;
+  grey: string;
+}
+
+export interface BreakPoints {
+  sm: string;
+  md: string;
+  lg: string;
+  xl: string;
+}
+
+export const fontSizes: FontSizes = {
+  xs: "10px",
+  sm: "12px",
+  md: "14px",
+  lg: "16px",
+  xl: "20px",
+};
+
+export const breakpoints: BreakPoints = {
+  sm: "600px",
+  md: "900px",
+  lg: "1200px",
+  xl: "1800px",
+};
+
+export const spaces: Space = {
+  xs: "4px",
+  sm: "8px",
+  md: "16px",
+  lg: "32px",
+  xl: "48px",
+};
+
+export const colors = {
+  black: "#231F20",
+  red: "#F14747",
+  blue: "#8CCFFA",
+  white: "#ffffff",
+  grey: "#EEEEEE",
+  darkblue: "#1F44A4",
+};
+
+export interface Theme {
+  colors: Colors;
+  fontSizes: FontSizes;
+  breakpoints: BreakPoints;
+  spaces: Space;
+}
+
+const theme: Theme = {
+  colors,
+  fontSizes,
+  breakpoints,
+  spaces,
+};
+
+export default theme;
