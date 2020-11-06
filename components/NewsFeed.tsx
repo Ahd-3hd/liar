eb7a6c0509b01932a8437e86200d906801abe108
ahd <ahdhani0@gmail.com>
2020-11-06 07:53:30 +0300

    chore: add avatar and username and question to newsfeedcard

--- components/NewsFeed.tsx
+++ components/NewsFeed.tsx
@@ -1,9 +1,37 @@
-import { Wrapper, Title } from "../styles/NewsFeed.style";
+import {
+  Wrapper,
+  Title,
+  CardsContainer,
+  NewsFeedCard,
+  NewsFeedPosterSection,
+  NewsFeedCommentsSection,
+  PosterAvatar,
+  PostContainer,
+  PosterName,
+  PosterQuestion,
+  AvatarContainer,
+} from "../styles/NewsFeed.style";
 
 export default function NewsFeed() {
   return (
     <Wrapper>
       <Title>Popular</Title>
+      <CardsContainer>
+        <NewsFeedCard>
+          <NewsFeedPosterSection>
+            <AvatarContainer>
+              <PosterAvatar src="/static/img/avatar.png" alt="avatar" />
+            </AvatarContainer>
+            <PostContainer>
+              <PosterName>john Doe</PosterName>
+              <PosterQuestion>
+                Tell me what you want what you really really want?
+              </PosterQuestion>
+            </PostContainer>
+          </NewsFeedPosterSection>
+          <NewsFeedCommentsSection></NewsFeedCommentsSection>
+        </NewsFeedCard>
+      </CardsContainer>
     </Wrapper>
   );
 }
--- styles/NewsFeed.style.tsx
+++ styles/NewsFeed.style.tsx
@@ -1,8 +1,35 @@
 import styled from "styled-components";
+import { Card } from "../components/Card";
 
 export const Wrapper = styled.section``;
 export const Title = styled.p`
   font-weight: ${({ theme: { fontWeights } }) => fontWeights.bold};
   color: ${({ theme: { colors } }) => colors.text};
   font-size: ${({ theme: { fontSizes } }) => fontSizes.md};
+`;
+export const CardsContainer = styled.div``;
+export const NewsFeedCard = styled(Card)`
+  padding: 0.5rem;
+  color: ${({ theme: { colors } }) => colors.text};
+`;
+export const NewsFeedPosterSection = styled.div`
+  display: flex;
+  align-items: flex-start;
+`;
+export const NewsFeedCommentsSection = styled.div``;
+export const AvatarContainer = styled.div`
+  margin-right: 1rem;
+`;
+export const PosterAvatar = styled.img`
+  width: 100%;
+  max-width: 100px;
+`;
+export const PostContainer = styled.div``;
+export const PosterName = styled.p`
+  font-size: ${({ theme: { fontSizes } }) => fontSizes.sm};
+  font-weight: ${({ theme: { fontWeights } }) => fontWeights.normal};
+`;
+export const PosterQuestion = styled.p`
+  font-weight: ${({ theme: { fontWeights } }) => fontWeights.bold};
+  line-height: 140%;
 `;
