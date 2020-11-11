b398fb44e1302b472bcc598383a82334bbde5723
ahd <ahdhani0@gmail.com>
2020-11-11 22:13:16 +0300

    feat: create the signup page

---
+++ pages/signup/index.tsx
@@ -0,0 +1,81 @@
+import {
+  Container,
+  Form,
+  Title,
+  CreateAccountLink,
+  InputGroup,
+  InputField,
+  InputLabel,
+} from "../../styles/login.style";
+import { Button } from "../../components/Buttons";
+import Link from "next/link";
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { auth } from "../../config/config";
+import {
+  clearCurrentUser,
+  setCurrentUser,
+} from "../../redux/actions/authActions";
+import { useRouter } from "next/router";
+
+export default function Signup() {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const currentUser = useSelector(
+    ({ auth }: { auth: any }) => auth.currentUser
+  );
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSignup = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    if (currentUser) router.push("/");
+  });
+
+  return (
+    <>
+      <Head>
+        <title>Signup</title>
+      </Head>
+      <Container>
+        <Title>Create a new account</Title>
+        <Form onSubmit={handleSignup}>
+          <InputGroup>
+            <InputLabel>Email</InputLabel>
+            <InputField
+              type="email"
+              onChange={(e) =>
+                setUserData((prevState) => ({
+                  ...prevState,
+                  email: e.target.value,
+                }))
+              }
+            />
+          </InputGroup>
+          <InputGroup>
+            <InputLabel>Password</InputLabel>
+            <InputField
+              type="password"
+              onChange={(e) =>
+                setUserData((prevState) => ({
+                  ...prevState,
+                  password: e.target.value,
+                }))
+              }
+            />
+          </InputGroup>
+          <Button type="submit">SIGNUP</Button>
+        </Form>
+        <Link href="/register" passHref>
+          <CreateAccountLink>already have an account?</CreateAccountLink>
+        </Link>
+      </Container>
+    </>
+  );
+}
