7117bf7108b58c620531dd9ad6776d700d32adbe
ahd <ahdhani0@gmail.com>
2020-11-19 01:28:23 +0300

    feat: add posts

--- components/NewQuestion.tsx
+++ components/NewQuestion.tsx
@@ -7,15 +7,39 @@ import {
 import { useState } from "react";
 import { Card } from "./Card";
 import { Button } from "./Buttons";
+import { addPost } from "../redux/posts/postsSlice";
+import { useDispatch, useSelector } from "react-redux";
+import firebase, { auth } from "../config/config";
 
 export default function NewQuestion() {
+  const { currentUser, isUserLoading, isUserFetchError } = useSelector(
+    (state: any) => state.auth
+  );
+  const dispatch = useDispatch();
   const [postData, setPostData] = useState({
     realQuestion: "",
     fakeQuestion: "",
   });
 
-  const addPostDispatch = (e: { preventDefault: () => void }) => {
+  const addPostDispatch = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
+    const newPost = {
+      userId: currentUser.userId,
+      email: currentUser.email,
+      realQuestion: postData.realQuestion,
+      fakeQuestion: postData.fakeQuestion,
+      isRevealed: false,
+      comments: [],
+      createdAt: firebase.firestore.Timestamp.now().seconds,
+      avatar: currentUser.avatar,
+    };
+    const docRef = await firebase.firestore().collection("posts").add(newPost);
+    dispatch(
+      addPost({
+        ...newPost,
+        id: docRef.id,
+      })
+    );
   };
 
   return (
--- redux/posts/postsSlice.ts
+++ redux/posts/postsSlice.ts
@@ -1,32 +1,39 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import firebase from "../../config/config";
 
-export const fetchPosts = createAsyncThunk("posts", async (userId?: string) => {
-  const fetchedPosts: any = [];
-  if (userId) {
-    const data = await firebase
-      .firestore()
-      .collection("posts")
-      .where("userId", "==", userId)
-      .orderBy("createdAt", "desc")
-      .get();
-    data.forEach((post) => {
-      const addId = { ...post.data(), id: post.id };
-      fetchedPosts.push(addId);
-    });
-    return fetchedPosts;
-  } else {
-    const data = await firebase
-      .firestore()
-      .collection("posts")
-      .orderBy("createdAt", "desc")
-      .get();
-    data.forEach((post) => {
-      const addId = { ...post.data(), id: post.id };
-      fetchedPosts.push(addId);
-    });
-    return fetchedPosts;
+export const fetchPosts = createAsyncThunk(
+  "fetchPosts",
+  async (userId?: string) => {
+    const fetchedPosts: any = [];
+    if (userId) {
+      const data = await firebase
+        .firestore()
+        .collection("posts")
+        .where("userId", "==", userId)
+        .orderBy("createdAt", "desc")
+        .get();
+      data.forEach((post) => {
+        const addId = { ...post.data(), id: post.id };
+        fetchedPosts.push(addId);
+      });
+      return fetchedPosts;
+    } else {
+      const data = await firebase
+        .firestore()
+        .collection("posts")
+        .orderBy("createdAt", "desc")
+        .get();
+      data.forEach((post) => {
+        const addId = { ...post.data(), id: post.id };
+        fetchedPosts.push(addId);
+      });
+      return fetchedPosts;
+    }
   }
+);
+
+export const addPost = createAsyncThunk("addPost", async (postData: any) => {
+  return postData;
 });
 
 const postsSlice = createSlice({
@@ -48,6 +55,9 @@ const postsSlice = createSlice({
     });
     builder.addCase(fetchPosts.rejected, (state, action) => {
       state.fetchError = true;
+    });
+    builder.addCase(addPost.fulfilled, (state: any, action) => {
+      state.posts = [action.payload, ...state.posts];
     });
   },
 });
