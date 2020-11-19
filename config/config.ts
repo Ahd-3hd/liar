bb29c74eb03adb2ab281302bfef840add140854f
ahd <ahdhani0@gmail.com>
2020-11-19 04:55:25 +0300

    chore: add analytics

--- config/config.ts
+++ config/config.ts
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
+import "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -19,7 +20,6 @@ var firebaseConfig = {
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
-// firebase.analytics();
 
 export const auth = firebase.auth();
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,13 @@ import GlobalStyles from "../utils/GlobalStyles";
 import Layout from "../components/Layout";
 import { Provider } from "react-redux";
 import store from "../redux/store";
+import { useEffect } from "react";
+import firebase from "../config/config";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    firebase.analytics();
+  }, []);
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
