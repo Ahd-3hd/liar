3c9850c876ae367235a32b5af71b57faf64a5705
ahd <ahdhani0@gmail.com>
2020-11-19 03:20:59 +0300

    feat: signup

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -15,19 +15,52 @@ import { auth } from "../../config/config";
 
 import { useRouter } from "next/router";
 import firebase from "../../config/config";
+import { setCurrentUser } from "../../redux/auth/authSlice";
+import { useDispatch, useSelector } from "react-redux";
 export default function Signup() {
+  const { currentUser, isUserLoading, isUserFetchError } = useSelector(
+    (state: any) => state.auth
+  );
   const router = useRouter();
-
+  const dispatch = useDispatch();
   const [userData, setUserData] = useState({
     email: "",
     password: "",
   });
 
-  const handleSignup = (e: { preventDefault: () => void }) => {
+  const handleSignup = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
+    const newUser = await auth.createUserWithEmailAndPassword(
+      userData.email,
+      userData.password
+    );
+    await firebase.firestore().collection("users").doc(newUser.user?.uid).set({
+      userId: newUser.user?.uid,
+      avatar:
+        "https://firebasestorage.googleapis.com/v0/b/liar-35d32.appspot.com/o/Group%2041.png?alt=media&token=49380a39-6c10-44bb-9481-eb7d7539a99f",
+      email: newUser.user?.email,
+      friends: [],
+    });
+    if (newUser) {
+      dispatch(
+        setCurrentUser({
+          userId: newUser.user?.uid,
+          avatar:
+            "https://firebasestorage.googleapis.com/v0/b/liar-35d32.appspot.com/o/Group%2041.png?alt=media&token=49380a39-6c10-44bb-9481-eb7d7539a99f",
+          email: newUser.user?.email,
+          friends: [],
+        })
+      );
+    } else {
+      dispatch(setCurrentUser(null));
+    }
   };
 
-  useEffect(() => {});
+  useEffect(() => {
+    if (currentUser) {
+      router.push("/login/");
+    }
+  });
 
   return (
     <>
